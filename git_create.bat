b64076fa4c7b4686e87dc9e197eef8e682cef113
MikhailBelkin <belkin.m@gmail.com>
2024-05-29 20:38:07 +0200

    v1

--- git_create.bat
+++
@@ -1,8 +0,0 @@
-echo "# cpp-find-unique-numbers-GPU" >> README.md
-git init
-git add README.md
-git add .
-git commit -m "v1"
-git branch -M main
-git remote add origin https://github.com/MikhailBelkin/cpp-find-unique-numbers-GPU.git
-git push -u origin main
